e9f52f3dfd3a9c9ee1aef374e91826cece233893
Nicole Uyigue-Sivek <nicoleusivek@gmail.com>
2020-09-03 21:10:51 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,8 +1,8 @@
-def find_even_values(src)
+def find_min_in_nested_arrays(array_of_daily_temperatures)
   row_index = 0
-  while row_index < src.count do
+  while row_index < array_of_daily_temperatures.count do
     element_index = 0
-    while element_index < src[row_index].count do
+    while element_index < array_of_daily_temperatures[row_index].count do
       # inside this loop, src[row_index][element_index] will access the current element
       element_index += 1
     end
